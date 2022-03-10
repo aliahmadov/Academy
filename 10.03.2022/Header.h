908d26ff13cf2a0b92edd9f6bd4a3bb3c5187530
Ali <ali123@mail.ru>
2022-03-10 19:43:17 +0400

    source and first header added

--- 10.03.2022/Header.h
+++ 10.03.2022/Header.h
@@ -1,1 +1,41 @@
 #pragma once
+#include<iostream>
+using namespace std;
+
+
+struct Teacher {
+	char* fullname;
+	char* speciality;
+	int age;
+	int id;
+};
+
+struct Director {
+	char* fullname;
+	char* speciality;
+	int age;
+	int id;
+};
+
+struct Student {
+
+	char* fullname;
+	int age;
+	int count;
+	int id;
+
+};
+
+struct Group {
+	char* title;
+	char* occupation;
+	
+	Student** students;
+};
+
+struct Academy {
+	Director** directors;
+	Group** groups;
+	Teacher** teachers;
+};
+
